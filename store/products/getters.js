e6ee4725401495b462393b50220d6af4397dbcb2
Fawaz Hutomi Abdurahman <hutomifawaza@gmail.com>
2024-10-11 10:32:48 +0700

    update: gitignore

--- store/products/getters.js
+++ store/products/getters.js
@@ -1,9 +1,8 @@
 export const getters = {
-    allProducts(state) {
-      return state.products;
-    },
-    getProductDetail(state) {
-      return state.selectedProduct;
-    },
-  };
-  
+  allProducts(state) {
+    return state.products;
+  },
+  getProductDetail(state) {
+    return state.selectedProduct;
+  },
+};
